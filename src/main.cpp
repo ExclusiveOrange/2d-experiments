d7933720c959c17dcc7c751f730c1d424d4d6de8
Atlee Brink <atlee@atleebrink.com>
2022-06-06 15:24:31 -0700

    loading and drawing png image

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <filesystem>
+#include <functional>
 #include <optional>
 #include <type_traits>
 #include <utility>
 
 #include <SDL.h>
+#include <SDL_image.h>
 
 #include "func.hpp"
 #include "toString.hpp"
@@ -13,28 +15,48 @@ namespace
 {
   using errmsg = std::optional<std::string>;
 
-  namespace imageFilenames
-  {
-    namespace terrainTiles
-    {
-      constexpr const char *test1 = "2-1 terrain tile 1.png";
-    }
-  }
+  const std::filesystem::path ASSETS = "assets";
+  const std::filesystem::path ASSETS_IMAGES = ASSETS / "images";
 
   struct Images
   {
-    SDL_Surface *test1;
+    static constexpr const char *test1_file = "2-1 terrain tile 1.png";
+    SDL_Surface *test1{};
 
     ~Images()
     {
-      SDL_FreeSurface(test1);
+      if(test1)
+        SDL_FreeSurface(test1);
     }
   };
 
-  errmsg
-  withSdlWindow(int width, int height, func<SDL_Window *,errmsg> auto &&f)
+  errmsg loadImage(const char *filename, SDL_Surface **dest)
   {
-    std::optional<std::string> errormsg{};
+    std::filesystem::path path = ASSETS_IMAGES / filename;
+
+    if(SDL_Surface *maybeSurface = IMG_Load(path.string().c_str()))
+      return (*dest = maybeSurface, std::nullopt);
+
+    return toString("IMG_Load failed! SDL_Error: ", SDL_GetError());
+  }
+
+  errmsg
+  withImages(func<const Images &, errmsg> auto &&f)
+  {
+    errmsg errormsg{};
+    Images images{};
+    bool allImagesLoaded = !(errormsg = loadImage(Images::test1_file, &images.test1));
+
+    if(allImagesLoaded)
+      return f(images);
+
+    return errormsg;
+  }
+
+  errmsg
+  withSdlWindow(int width, int height, func<SDL_Window *, errmsg> auto &&f)
+  {
+    errmsg errormsg{};
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
       errormsg = toString("SDL_Init failed! SDL_Error: ", SDL_GetError());
@@ -62,22 +84,29 @@ namespace
   }
 
   errmsg
-  withImages(func<const Images &, errmsg> auto &&f)
+  runWithWindowAndImages(SDL_Window *sdlWindow, Images &images)
   {
-    // TODO
+    auto surface = SDL_GetWindowSurface(sdlWindow);
+
+    SDL_BlitSurface(images.test1, nullptr, surface, nullptr);
+    SDL_UpdateWindowSurface(sdlWindow);
+
+    // DELETE
+    SDL_Delay(2000);
+
+    return {};
   }
 
-  std::optional<std::string>
-  run(SDL_Window *sdlWindow)
+  errmsg
+  runWithWindow(SDL_Window *sdlWindow)
   {
     SDL_Surface *sdlSurface = SDL_GetWindowSurface(sdlWindow);
 
+    // load screen I guess
     SDL_FillRect(sdlSurface, nullptr, SDL_MapRGB(sdlSurface->format, 255, 255, 255));
-
     SDL_UpdateWindowSurface(sdlWindow);
-    SDL_Delay(2000);
 
-    return std::nullopt;
+    return withImages(std::bind(runWithWindowAndImages, sdlWindow, std::placeholders::_1));
   }
 }
 
@@ -85,7 +114,7 @@ int main(int argc, char *argv[])
 {
   constexpr int width = 1920, height = 1080;
 
-  if(std::optional<std::string> errormsg = withSdlWindow(width, height, run))
+  if(errmsg errormsg = withSdlWindow(width, height, runWithWindow))
     std::cerr << "error: " << *errormsg << std::endl;
 
   return 0;
