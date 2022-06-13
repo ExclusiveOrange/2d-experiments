f2452950db5f1312009d13b3d67e007927633ac2
Atlee Brink <ExclusiveOrange@users.noreply.github.com>
2022-06-12 19:41:33 -0700

    had to adjust template constraints for Mac clang because it didn't understand std::integral concept used as a constraint

--- src/clip.hpp
+++ src/clip.hpp
@@ -2,10 +2,10 @@
 
 #include <concepts>
 
-template<class T> requires std::integral<T> || std::floating_point<T>
+template<class T> requires std::is_integral_v<T> || std::is_floating_point_v<T>
 constexpr T
 clipMin(T destx, T destw, T srcw) {return destx < 0 ? -destx : 0;}
 
-template<class T> requires std::integral<T> || std::floating_point<T>
+template<class T> requires std::is_integral_v<T> || std::is_floating_point_v<T>
 constexpr T
 clipMax(T destx, T destw, T srcw) {return (destw - destx) < srcw ? (destw - destx) : srcw;}
