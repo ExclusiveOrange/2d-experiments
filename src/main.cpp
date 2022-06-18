82cb35c252f79f9df818bcbdecca3e8905f3d1fb
Atlee Brink <atlee@atleebrink.com>
2022-06-18 00:12:18 -0700

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -526,47 +526,37 @@ namespace testing
     }
   };
 
+  #if defined(__GCC__) || defined(__clang__)
   uint32_t blendArgb(uint32_t argb0, uint32_t argb1, uint8_t t)
   {
-    //union Prism {struct {uint8_t a, r, g, b;}; uint32_t argb;} p0{.argb = argb0}, p1{.argb = argb1};
-
-
     // GCC extension for vectors, also supported by Clang (not sure min version but clang-cl seems okay with it).
-    typedef uint8_t uint8_4 __attribute__((vector_size(4 * sizeof(uint8_t))));
-    typedef uint16_t uint16_4 __attribute__((vector_size(4 * sizeof(uint16_t))));
+    // This version is much faster than the non-vector version below at least on compilers which support it.
 
-    union Prism2 {uint32_t u32; uint8_4 u8_4;};
-    Prism2 a8{.u32 = argb0}, b8{.u32 = argb1};
+    using uint8_4 = uint8_t __attribute__((vector_size(4 * sizeof(uint8_t))));
+    using uint16_4 = uint16_t __attribute__((vector_size(4 * sizeof(uint16_t))));
+
+    union {uint32_t u32; uint8_4 u8_4;}
+      a8{.u32 = argb0}, b8{.u32 = argb1}, r;
+
     uint16_4 a16{__builtin_convertvector(a8.u8_4, uint16_4)};
     uint16_4 b16{__builtin_convertvector(b8.u8_4, uint16_4)};
-
-    uint16_4 c16{a16 * uint16_t(255 - t)};
-    uint16_4 d16{b16 * uint16_t(t)};
-
-    uint16_4 e16{c16 + d16};
-    uint16_4 f16{e16 / uint16_t(255)};
-
-    uint8_4 f8{__builtin_convertvector(f16, uint8_4)};
-
-    Prism2 r{.u8_4 = f8};
+    uint16_4 f16{(a16 * uint16_t(255 - t) + b16 * uint16_t(t)) / uint16_t(255)};
+    r.u8_4 = __builtin_convertvector(f16, uint8_4);
 
     return r.u32;
-
-    // tried glm in case it would use AVX but if it did it wasn't any faster
-    //constexpr float r255 = 1.f / 255.f;
-    //glm::u8vec4 v8_r{glm::mix(glm::vec4{p0.a, p0.r, p0.g, p0.b}, glm::vec4{p1.a, p1.r, p1.g, p1.b}, (float)t * r255)};
-    //Prism r{.a = v8_r.x, .r = v8_r.y, .g = v8_r.z, .b = v8_r.w};
-
-    // MSVC compiler does not optimize this at all unfortunately.
-    // Probably will have to manually use SIMD intrinsics for this, if there's a way.
-    //Prism r{
-    //  .a = uint8_t(((uint16_t)p0.a * (255 - t) + (uint16_t)p1.a * t) / 255),
-    //  .r = uint8_t(((uint16_t)p0.r * (255 - t) + (uint16_t)p1.r * t) / 255),
-    //  .g = uint8_t(((uint16_t)p0.g * (255 - t) + (uint16_t)p1.g * t) / 255),
-    //  .b = uint8_t(((uint16_t)p0.b * (255 - t) + (uint16_t)p1.b * t) / 255)};
-    //
-    //return r.argb;
   };
+  #else // not defined(__GCC__) and not defined(__clang__)
+  uint32_t blendArgb(uint32_t argb0, uint32_t argb1, uint8_t t)
+  {
+    // This operation is much slower with MSVC for some reason. I tried several variations and they were all bad compared to clang-cl.
+    union Prism {struct {uint8_t a, r, g, b;}; uint32_t argb;} p0{.argb = argb0}, p1{.argb = argb1};
+    return Prism{
+      .a = uint8_t(((uint16_t)p0.a * uint16_t(255 - t) + (uint16_t)p1.a * uint16_t(t)) / 255),
+      .r = uint8_t(((uint16_t)p0.r * uint16_t(255 - t) + (uint16_t)p1.r * uint16_t(t)) / 255),
+      .g = uint8_t(((uint16_t)p0.g * uint16_t(255 - t) + (uint16_t)p1.g * uint16_t(t)) / 255),
+      .b = uint8_t(((uint16_t)p0.b * uint16_t(255 - t) + (uint16_t)p1.b * uint16_t(t)) / 255)}.argb;
+  }
+  #endif
 }
 
 //======================================================================================================================
